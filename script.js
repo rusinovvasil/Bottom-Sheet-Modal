6b332e079301edd811f4dc08ccb70b0882ba31e4
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-05-10 19:17:55 +0200

    Bottom sheet Modal
    Create a draggable bottom sheet modal functions similarly to a Facebook modal using HTML, CSS, and Javascript.

--- script.js
+++ script.js
@@ -4,22 +4,57 @@ const sheetOverlay = bottomSheet.querySelector(".sheet-overlay");
 const sheetContent = bottomSheet.querySelector(".content");
 const dragIcon = bottomSheet.querySelector(".drag-icon");
 
-let isDragging = false;
+// Global variables for tracking drag events
+let isDragging = false, startY, startHeight;
 
 const showBottomSheet = () => {
     bottomSheet.classList.add("show");
+    document.body.style.overflowY = "hidden";
+    updateSheetHeight(50);
 }
 const hideBottomSheet = () => {
     bottomSheet.classList.remove("show");
+    document.body.style.overflowY = "auto";
 }
 
+const updateSheetHeight = (height) => {
+    sheetContent.style.height = `${height}vh`; // updates the height of the sheet content
+    // Toggles the fullscreen class to bottomSheet if the height is equal to 100
+    bottomSheet.classList.toggle("fullscreen", height === 100);
+}
+
+// Sets initial drag position and sheetContent height
+const dragStart = (e) => {
+    isDragging = true;
+    startY = e.pageY || e.touches?.[0].pageY;
+    startHeight = parseInt(sheetContent.style.height);
+    bottomSheet.classList.add("dragging");
+}
+
+// Calculates the new height for the sheet content and call the updateSheetHeight function
 const dragging = (e) => {
     if (!isDragging) return;
-    sheetContent.style.height = `${e.pageY}vh`;
-    console.log(e.pageY)
+    const delta = startY - (e.pageY || e.touches?.[0].pageY);
+    const newHeight = startHeight + delta / window.innerHeight * 100;
+    updateSheetHeight(newHeight);
 }
 
-dragIcon.addEventListener("mousemove", dragging);
+// Determines whether to hide, set to fullscreen, or set to default
+// height based on the current height of the sheet content
+const dragStop = () => {
+    isDragging = false;
+    bottomSheet.classList.remove("dragging");
+    const sheetHeight = parseInt(sheetContent.style.height);
+    sheetHeight < 25 ? hideBottomSheet() : sheetHeight > 75 ? updateSheetHeight(100) : updateSheetHeight(50);
+}
+
+document.addEventListener("mouseup", dragStop);
+dragIcon.addEventListener("mousedown", dragStart);
+document.addEventListener("mousemove", dragging);
+
+document.addEventListener("touchend", dragStop);
+dragIcon.addEventListener("touchstart", dragStart);
+document.addEventListener("touchmove", dragging);
 
 showModalBtn.addEventListener("click", showBottomSheet);
 sheetOverlay.addEventListener("click", hideBottomSheet);
